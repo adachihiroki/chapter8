1af147b68642793589e983f19e3496263bc419c2
AdachiHiroki <241039@std.hi-joho.ac.jp>
2025-06-11 09:26:01 +0900

    chapter8

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,16 @@
 public class Hero {
     String name;
     int hp;
+    Sword sword;
+
+    public void attack(){
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に5ポイントのダメージをあたえた！");
+    }
+    public Hero(String name){
+        this.hp =100;
+        this.name = name;
+    }
     public void sleep(){
         this.hp = 100;
         System.out.println(this.name + "は、眠って回復した!");
@@ -19,6 +29,10 @@ public class Hero {
         System.out.println(this.name + "は、逃げ出した");
         System.out.println( "GAMEOVER");
         System.out.println("最終HPは" + this.hp + "でした");
+
+    }
+    public Hero(){
+        this.hp = 100;
     }
 }
 
